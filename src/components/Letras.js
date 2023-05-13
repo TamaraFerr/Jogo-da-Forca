0e444314efaf8f3d011d2f9ab5554f7432efb665
TamaraFerr <tamarablacksmit@hotmail.com>
2023-05-12 22:10:28 -0300

    iniciando a lógica

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 
 export default function Letras(){
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+    function selectLetter(){
+        console.log("Letra selecionada!")
+        
+    }
     
     return(
         <ul className="letters">
             {alfabeto.map(letra => (
-                <li className="letter" data-test="letter">{letra}</li>
+                <li className="letter" 
+                    data-test="letter" 
+                    onClick={selectLetter}
+                    disable={true}
+                    >
+                        {letra}
+                </li>
             ))}
         </ul>
     )
